0ecc9ac38012132e2137c8ab52700d530b12a9e9
g3andre <g3tinho@hotmail.com>
2021-11-15 19:18:11 -0300

    Adicionado UseCase ListAllUsers

--- src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
@@ -1,12 +1,17 @@
-import { Request, Response } from "express";
+import { Request, Response } from 'express';
 
-import { ListAllUsersUseCase } from "./ListAllUsersUseCase";
+import { ListAllUsersUseCase } from './ListAllUsersUseCase';
 
 class ListAllUsersController {
   constructor(private listAllUsersUseCase: ListAllUsersUseCase) {}
 
   handle(request: Request, response: Response): Response {
-    // Complete aqui
+    const { user_id } = request.headers;
+    const users = this.listAllUsersUseCase.execute({
+      user_id: user_id as string,
+    });
+
+    return response.status(200).json(users);
   }
 }
 

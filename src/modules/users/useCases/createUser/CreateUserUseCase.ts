bc7783f156befc3a76bbb1fc1e72fe24bed7dbb7
g3andre <g3tinho@hotmail.com>
2021-11-15 18:48:03 -0300

    Adicionado UseCase CreateUser

--- src/modules/users/useCases/createUser/CreateUserUseCase.ts
+++ src/modules/users/useCases/createUser/CreateUserUseCase.ts
@@ -1,5 +1,5 @@
-import { User } from "../../model/User";
-import { IUsersRepository } from "../../repositories/IUsersRepository";
+import { User } from '../../model/User';
+import { IUsersRepository } from '../../repositories/IUsersRepository';
 
 interface IRequest {
   name: string;
@@ -10,7 +10,13 @@ class CreateUserUseCase {
   constructor(private usersRepository: IUsersRepository) {}
 
   execute({ email, name }: IRequest): User {
-    // Complete aqui
+    let user = this.usersRepository.findByEmail(email);
+    if (user) {
+      throw new Error('User already exists...');
+    }
+
+    user = this.usersRepository.create({ name, email });
+    return user;
   }
 }
 
